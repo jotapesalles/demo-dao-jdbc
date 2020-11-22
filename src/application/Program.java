c5c5b1491a3a408469b875c293e796c84a02d7dc
jotapesalles <joaopaulo_jpds@hotmail.com>
2020-11-22 19:18:35 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,16 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
+		Seller sel = new Seller(21, "Bob", "bob@gmail.com", "25/05/1998", 3000.0, obj );
 		System.out.println(obj);
-
+		System.out.println(sel);
 	}
 
 }
